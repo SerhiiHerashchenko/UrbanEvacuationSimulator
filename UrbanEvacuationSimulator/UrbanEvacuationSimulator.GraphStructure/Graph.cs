8ca0217e4a4364aa4d7a16c6b5da9ab3871b136b
Serhii Herashchenko <serhii.herashchenko@stud.onu.edu.ua>
2026-02-02 19:35:20 +0200

    Added and partially implemented Vertex and Craph classes

---
+++ UrbanEvacuationSimulator/UrbanEvacuationSimulator.GraphStructure/Graph.cs
@@ -0,0 +1,30 @@
+﻿namespace UrbanEvacuationSimulator.GraphStructure
+{
+    public class Graph<T>
+    {
+        private Dictionary<T, HashSet<T>> adjacencyList;
+
+        public Graph() => adjacencyList = new Dictionary<T, HashSet<T>>();
+        
+        public void AddVertex(T vertex)
+        {
+            if (!adjacencyList.ContainsKey(vertex))
+            {
+                adjacencyList[vertex] = new HashSet<T>();
+            }
+        }
+        
+        public void AddEdge(T source, T destination, bool bidirectional = false)
+        {
+            AddVertex(source);
+            AddVertex(destination);
+            
+            adjacencyList[source].Add(destination);
+            
+            if (bidirectional)
+            {
+                adjacencyList[destination].Add(source);
+            }
+        }
+    }
+}
---
+++ UrbanEvacuationSimulator/UrbanEvacuationSimulator.GraphStructure/Structures/Vertex.cs
@@ -0,0 +1,14 @@
+﻿namespace UrbanEvacuationSimulator.GraphStructure.Structures
+{
+    public readonly record struct Vertex
+    {
+        public double Lon { get; }
+        public double Lat { get; }
+
+        public Vertex(double lon, double lat)
+        {
+            Lon = lon;
+            Lat = lat;
+        }
+    }
+}
