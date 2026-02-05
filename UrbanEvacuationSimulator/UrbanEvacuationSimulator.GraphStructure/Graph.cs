755f6d8a88bd0cd637e8f328020c51b0604363b2
Serhii Herashchenko <serhii.herashchenko@stud.onu.edu.ua>
2026-02-05 21:28:48 +0200

    Implemented FindPathAStar() Graph method and GetDistance() Vertex method

--- UrbanEvacuationSimulator/UrbanEvacuationSimulator.GraphStructure/Graph.cs
+++ UrbanEvacuationSimulator/UrbanEvacuationSimulator.GraphStructure/Graph.cs
@@ -1,11 +1,11 @@
 ﻿namespace UrbanEvacuationSimulator.GraphStructure
 {
-    public class Graph<T>
+    public class Graph<T> where T : notnull
     {
         private Dictionary<T, HashSet<T>> adjacencyList;
 
         public Graph() => adjacencyList = new Dictionary<T, HashSet<T>>();
-        
+
         public void AddVertex(T vertex)
         {
             if (!adjacencyList.ContainsKey(vertex))
@@ -20,11 +20,88 @@
             AddVertex(destination);
             
             adjacencyList[source].Add(destination);
-            
+
             if (bidirectional)
             {
                 adjacencyList[destination].Add(source);
             }
+        }
+        
+        public List<T> FindPathAStar(
+            T start, 
+            T destination, 
+            Func<T, T, double> heuristic, 
+            Func<T, T, double> costFunction)
+        {
+            if (!adjacencyList.ContainsKey(start) ||
+                !adjacencyList.ContainsKey(destination))
+            {
+                return null;
+            }
+
+            var openSet = new PriorityQueue<T, double>();
+            openSet.Enqueue(start, 0);
+
+            var cameFrom = new Dictionary<T, T>();
+            var gScore = new Dictionary<T, double>();
+            gScore[start] = 0;
+            
+            var fScore = new Dictionary<T, double>();
+            fScore[start] = heuristic(start, destination);
+
+            var openSetHash = new HashSet<T> { start };
+
+            while (openSet.Count > 0)
+            {
+                T current = openSet.Dequeue();
+                openSetHash.Remove(current);
+
+                if (EqualityComparer<T>.Default.Equals(current, destination))
+                {
+                    return ReconstructPath(cameFrom, current);
+                }
+                
+                if (adjacencyList.TryGetValue(current, out var neighbors))
+                {
+                    foreach (var neighbor in neighbors)
+                    {
+                        double weight = costFunction(current, neighbor);
+                        
+                        double tentativeGScore = gScore[current] + weight;
+
+                        if (!gScore.ContainsKey(neighbor) || tentativeGScore < gScore[neighbor])
+                        {
+                            cameFrom[neighbor] = current;
+                            gScore[neighbor] = tentativeGScore;
+                            double f = tentativeGScore + heuristic(neighbor, destination);
+                            fScore[neighbor] = f;
+
+                            if (!openSetHash.Contains(neighbor))
+                            {
+                                openSet.Enqueue(neighbor, f);
+                                openSetHash.Add(neighbor);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private List<T> ReconstructPath(Dictionary<T, T> cameFrom, T current)
+        {
+            var totalPath = new List<T>();
+            totalPath.Add(current);
+            
+            while (cameFrom.ContainsKey(current))
+            {
+                current = cameFrom[current];
+                totalPath.Add(current);
+            }
+            
+            totalPath.Reverse();
+            return totalPath;
         }
     }
 }
--- UrbanEvacuationSimulator/UrbanEvacuationSimulator.GraphStructure/Structures/Vertex.cs
+++ UrbanEvacuationSimulator/UrbanEvacuationSimulator.GraphStructure/Structures/Vertex.cs
@@ -2,6 +2,8 @@
 {
     public readonly record struct Vertex
     {
+        private const double EarthRadiusKm = 6371.0;
+        
         public double Lon { get; }
         public double Lat { get; }
 
@@ -9,6 +11,27 @@
         {
             Lon = lon;
             Lat = lat;
+        }
+        
+        public static double GetDistance(Vertex a, Vertex b)
+        {
+            var dLat = DegreesToRadians(b.Lat - a.Lat);
+            var dLon = DegreesToRadians(b.Lon - a.Lon);
+
+            var lat1 = DegreesToRadians(a.Lat);
+            var lat2 = DegreesToRadians(b.Lat);
+
+            var x = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                    Math.Sin(dLon / 2) * Math.Sin(dLon / 2) * Math.Cos(lat1) * Math.Cos(lat2);
+        
+            var d = 2 * Math.Asin(Math.Sqrt(x));
+
+            return EarthRadiusKm * d;
+        }
+
+        private static double DegreesToRadians(double degrees)
+        {
+            return degrees * Math.PI / 180.0;
         }
     }
 }
